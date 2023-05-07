e252564673801f21552914a5be31f8bf02cae1e9
Teinturier <henri.teinturier@gmail.com>
2023-05-07 18:53:23 +0200

    Step 1 fetching Data with useQuery

--- src/components/RQSuperHeroesPage.tsx
+++ src/components/RQSuperHeroesPage.tsx
@@ -1,6 +1,27 @@
+import axios from "axios";
+import { useQuery } from "react-query";
+import { Hero } from "../models/models";
+
+const fetchSuperHeroes = async () => {
+  return axios.get('http://localhost:4000/superheroes');
+};
+
 export const RQSuperHeroesPage = () => {
-  return  
+
+  const { isLoading, data } = useQuery('superheroes', fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>loading...</h2>
+  }
+
+  return  (
     <>
       <h1>React-Query Super Heroes Page</h1>
+      {
+        data?.data.map( (hero: Hero) => 
+           <div>{hero.name}</div>
+        )
+      }
     </>
+  )
 };
