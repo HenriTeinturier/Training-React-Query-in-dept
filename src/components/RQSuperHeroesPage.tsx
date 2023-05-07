3c38be90623ef6a9ae8c66e13c30251f091efe0a
Teinturier <henri.teinturier@gmail.com>
2023-05-07 19:08:08 +0200

    step 4 Handling Query Error

--- src/components/RQSuperHeroesPage.tsx
+++ src/components/RQSuperHeroesPage.tsx
@@ -3,23 +3,30 @@ import { useQuery } from "react-query";
 import { Hero } from "../models/models";
 
 const fetchSuperHeroes = async () => {
-  return axios.get('http://localhost:4000/superheroes');
+  return axios.get('http://localhost:40002/superheroes');
 };
 
 export const RQSuperHeroesPage = () => {
 
-  const { isLoading, data } = useQuery('superheroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery('superheroes', fetchSuperHeroes);
+
+  console.log(error)
+  console.log(isError)
 
   if (isLoading) {
     return <h2>loading...</h2>
   }
+
+  if (isError) {
+    return <h2>{error.message}</h2>
+  } 
 
   return  (
     <>
       <h1>React-Query Super Heroes Page</h1>
       {
         data?.data.map( (hero: Hero) => 
-           <div>{hero.name}</div>
+          <div>{hero.name}</div>
         )
       }
     </>
--- src/components/SuperHeroesPage.tsx
+++ src/components/SuperHeroesPage.tsx
@@ -5,17 +5,26 @@ import { Hero } from "../models/models";
 export const SuperHeroesPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<Hero[]>([]);
+  const [error, setError] = useState<string>('');
 
   useEffect(() => {
     axios.get('http://localhost:4000/superheroes')
     .then((response) => {
       setData(response.data);
       setIsLoading(false);
+    })
+    .catch((error) => {
+      setError(error.message);
+      setIsLoading(false);
     });
   }, []);
 
   if (isLoading) {
     return <h2>loading...</h2>
+  }
+
+  if (error) {
+    return <h2>{error}</h2>
   }
 
 
