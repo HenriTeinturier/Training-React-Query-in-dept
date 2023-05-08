e6af0a201de182491e4c513b5c13a449984557c2
Teinturier <henri.teinturier@gmail.com>
2023-05-08 12:10:07 +0200

    step 8 refetchOnMount / refetchOnWindowsFocus

--- src/components/RQSuperHeroesPage.tsx
+++ src/components/RQSuperHeroesPage.tsx
@@ -6,18 +6,15 @@ const fetchSuperHeroes = async () => {
   return axios.get('http://localhost:4000/superheroes');
 };
 
-  //by default the query cache time is 5 minnutes.
-  //the defaukt staleTime is 0 seconds
 
 export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
-      cacheTime: 5000, // change cacheTime to 5s. After 5s the query becomes inactive.
-      staleTime: 10000, // time without call from react-query. Only cache data is used.
-      // cache can be more longer than staleTime. We keep in cache the data and call only when staleTime is reached.
-      //* the query has a fresh tag when staleTime is not reached. after staleTime is reached, the query has a stale tag.
+      // cacheTime: default = 5minutes
+      // staleTime: default = 0s
+      refetchOnMount: false, // if true (default value), the query is called each time the component is mounted/rendered. If false, the query is called only when the component is mounted.
     }
     );
 
