38a98932c739159e5fa06c4b641a84c19952755a
Teinturier <henri.teinturier@gmail.com>
2023-05-08 11:53:10 +0200

    step 7 staleTime for define if call is necessary

--- src/components/RQSuperHeroesPage.tsx
+++ src/components/RQSuperHeroesPage.tsx
@@ -7,14 +7,17 @@ const fetchSuperHeroes = async () => {
 };
 
   //by default the query cache time is 5 minnutes.
+  //the defaukt staleTime is 0 seconds
 
 export const RQSuperHeroesPage = () => {
   const { isLoading, data, isError, error, isFetching } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
-      cacheTime: 5000, 
-      // change cacheTime to 5s. After 5s the query becomes inactive.
+      cacheTime: 5000, // change cacheTime to 5s. After 5s the query becomes inactive.
+      staleTime: 10000, // time without call from react-query. Only cache data is used.
+      // cache can be more longer than staleTime. We keep in cache the data and call only when staleTime is reached.
+      //* the query has a fresh tag when staleTime is not reached. after staleTime is reached, the query has a stale tag.
     }
     );
 
